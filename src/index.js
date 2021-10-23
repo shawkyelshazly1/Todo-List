70d5db6c5527e5688ce03235a5aae1c7fa7579ca
Shaq <shawkyelshazly2@gmail.com>
2021-10-23 22:02:07 +0200

    Inital Project Structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+document.querySelector(".add_new").addEventListener("click", (e) => {
+  document.querySelector(".colors").classList.toggle('open');
+  document.querySelector(".colors").classList.toggle('display');
+    
+  });
+
